a3ec9c4548e01be3516cd9e75777ff5c493cee4c
M Faris Alkodri <farisalkodri@gmail.com>
2021-01-18 11:32:37 +0700

    add package

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "warn",
+    "arrow-body-style": "off",
+    "prefer-arrow-callback": "off",
+  },
+};
