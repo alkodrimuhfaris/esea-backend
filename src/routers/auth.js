58ce5291014e09e500d30a1a4aab1521f5dd5b69
M Faris Alkodri <farisalkodri@gmail.com>
2021-01-20 23:51:49 +0700

    change auth router

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,8 +4,8 @@ const auth = require("../controllers/auth");
 
 // const forgotPassword = require("../controllers/forgotPassword");
 
-router.post("/login/:role_id", auth.loginController);
-router.post("/signup/:role_id", auth.signupController);
+router.post("/login", auth.loginController);
+router.post("/signup", auth.signupController);
 // router.post("/forgot", forgotPassword.resetPassword);
 // router.post("/forgot/reset", forgotPassword.matchResetCode);
 
