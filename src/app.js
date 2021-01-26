089a7fd29a0478102b889a7ea897f95a1506748f
M Faris Alkodri <farisalkodri@gmail.com>
2021-01-26 09:12:39 +0700

    add io.origin to handle CORS

--- src/app.js
+++ src/app.js
@@ -19,6 +19,7 @@ app.get("/", (req, res) => {
 // socket connection
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {});
+io.origins("*:*");
 module.exports = io;
 
 const clientSocket = [];
